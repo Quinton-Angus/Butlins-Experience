5108c0f229e50616c5b339339b151d03392981a5
Quinton Angus <quinton@quintondev.com>
2026-07-24 03:17:39 +0100

    Remove manual deep link listener from src layout

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -2,14 +2,10 @@ import { Stack } from "expo-router";
 import { useFonts } from 'expo-font'
 import { Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
-import * as linking from 'expo-linking'
-import deepLink from '../../scripts/deepLink'
 
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-
   const [fontsLoaded] = useFonts({
     Inter_300Light,
     Inter_400Regular,
@@ -18,10 +14,6 @@ export default function RootLayout() {
     Inter_700Bold,
     Inter_800ExtraBold,
   })
-
-  useEffect(() => {
-    const subscription = linking.addEventListener('url', ({url}) => { deepLink(url) })
-  }, [])
 
   return (
     <Stack
