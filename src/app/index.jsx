c618ecd70a958dccd5bcd8c6ebcee3a60a74b269
Quinton Angus <quinton@quintondev.com>
2026-07-24 03:17:35 +0100

    Handle connectionToken from index route

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,13 +4,15 @@ import SplashIcon from '../../Assets/splash/splashIcon.png'
 import { LinearGradient } from 'expo-linear-gradient'
 import { NavigationBar } from 'expo-navigation-bar'
 import { useEffect } from 'react'
+import { useLocalSearchParams } from 'expo-router'
 import UserRouter from '../../scripts/router.js'
 
 export default function splashScreen() {
+  const { connectionToken } = useLocalSearchParams()
 
   useEffect(() => {
-    UserRouter()
-  })
+    UserRouter(connectionToken)
+  }, [connectionToken])
 
   return (
     <LinearGradient style={Styles.container} start={{x:0, y:0}} end={{x:1, y:1}} colors={['#640707', '#DB2C2C', '#5e0909']}>
