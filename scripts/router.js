9ad667e8263020686a4d550776f2be92230ea6de
Quinton Angus <quinton@quintondev.com>
2026-07-24 03:17:30 +0100

    Fix deep link handling in src app router

--- scripts/router.js
+++ scripts/router.js
@@ -1,37 +1,55 @@
 import * as secureStore from 'expo-secure-store'
 import { router } from 'expo-router'
 
-export default async function route() {
+export default async function route(connectionToken) {
+  function welcome() {
+    router.replace('/welcome')
+  }
 
-    function welcome() {
-        router.replace('/welcome')
+  function holidaysOverview() {
+    router.replace('/holidaysOverview')
+  }
+
+  if (connectionToken) {
+    const appTokenRequest = await fetch('https://api.quintondev.com/butlins-experience/app/connect', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        connectionToken,
+      }),
+    })
+
+    const appTokenResponse = await appTokenRequest.json()
+
+    if (appTokenResponse.outcome === true) {
+      await secureStore.setItemAsync('appToken', appTokenResponse.appToken)
+      holidaysOverview()
+    } else {
+      welcome()
     }
 
-    function holidaysOverview() {
-        router.replace('/holidaysOverview')
+    return
+  }
+
+  const appToken = await secureStore.getItemAsync('appToken')
+
+  if (appToken) {
+    const verifyToken = await fetch('https://api.quintondev.com/butlins-experience/app/tokenverify', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        appToken,
+      }),
+    })
+
+    const verifyTokenResponse = await verifyToken.json()
+
+    if (verifyTokenResponse.outcome === true) {
+      holidaysOverview()
+    } else {
+      welcome()
     }
-
-    const appToken = await secureStore.getItemAsync('appToken')
-
-    if (appToken) {
-
-        const verifyToken = await fetch('https://api.quintondev.com/butlins-experience/app/tokenverify', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                appToken: appToken
-            })
-        })
-
-        const verifyTokenResponse = await verifyToken.json()
-
-        if (verifyTokenResponse.outcome === true) {
-            holidaysOverview()
-        } else {
-            welcome()
-        }
-    } else if () {
-        welcome()
-    }
-
+  } else {
+    welcome()
+  }
 }
